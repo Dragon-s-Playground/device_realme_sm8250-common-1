9f8dedde3a13f56ddea6eed0aca9081b8356fdb2
bibindasb <bibindasappu1@gmail.com>
2023-08-19 13:45:13 +0530

    Revert "sm8250-common: Fix light sensor"
    This reverts commit e89e6b9ce459b65ed7fc5cef17c6579f305c3a55.

--- sensors-multihal/HalProxy.cpp
+++ sensors-multihal/HalProxy.cpp
@@ -493,10 +493,6 @@ void HalProxy::initializeSensorList() {
                     ALOGV("Loaded sensor: %s", sensor.name.c_str());
                     sensor.sensorHandle = setSubHalIndex(sensor.sensorHandle, subHalIndex);
                     setDirectChannelFlags(&sensor, mSubHalList[subHalIndex]);
-                    if (sensor.typeAsString == "qti.sensor.wise_light") {
-                        sensor.type = SensorType::LIGHT;
-                        sensor.typeAsString = "";
-                    }
                     mSensors[sensor.sensorHandle] = sensor;
                 }
             }
