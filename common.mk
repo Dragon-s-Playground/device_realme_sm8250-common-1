aebec8723d67667b130923bcad52f14e132d3a5d
bibindasb <bibindasappu1@gmail.com>
2023-08-23 10:50:28 +0530

    Revert "sm8250-common: Import sensors/2.0/multihal"
    This reverts commit 249887b16b7cf1623c91558a79f8b0205d593b2e.

--- common.mk
+++ common.mk
@@ -434,9 +434,7 @@ PRODUCT_COPY_FILES += \
 
 # Sensors
 PRODUCT_PACKAGES += \
-    android.frameworks.sensorservice@1.0 \
-    android.frameworks.sensorservice@1.0.vendor \
-    android.hardware.sensors@2.1-service.multihal_oplus \
+    android.hardware.sensors@2.0-service.multihal \
     libsensorndkbridge \
     sensors.oplus
 
